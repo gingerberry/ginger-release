c1761d8d5351fe292f37ad29d28233a0a470e196
Ivan Hristov <ivan.hristov@sap.com>
2020-06-13 21:27:24 +0300

    Fix deploy_bottom

--- deploy_bottom.sh
+++ deploy_bottom.sh
@@ -1,22 +1,45 @@
 #!/usr/bin/env bash
-
+set -euo pipefail
 # Install php and apache2 server.
+echo "=============="
+echo "Installing php"
+echo "=============="
 apt-get update
 apt-get install apache2 <<< "Y"
 apt-get install php libapache2-mod-php <<< "Y"
 
 # Configure php and apache2.
+echo "========================="
+echo "Configuring prerequisites"
+echo "========================="
+
 a2enmod rewrite
 a2enmod headers
 
 TMP_FILE="$(mktemp tmp.XXXXXXX)"
 sed -E 's/(AllowOverride) None/\1 All/g' < /etc/apache2/apache2.conf > "$TMP_FILE"
 cat "$TMP_FILE" > /etc/apache2/apache2.conf
+rm "$TMP_FILE"
 
 sudo apt-get install php7.2-mysql
 sudo apt install php-simplexml
 
 systemctl restart apache2
+
+echo "================="
+echo "Installing ffmpeg"
+echo "================="
+
+PATH="$PATH:/snap/bin/ffmpeg"
+snap install ffmpeg
+# shellcheck disable=SC1090
+source ~/.bashrc
+
+echo "====================="
+echo "Deploying application"
+echo "====================="
+
+find /var/www/html -xdev -mindepth 1 -printf "%d\t%y\t%p\0" | sort -z -r -n | cut -z -f3- | xargs -0 -r -- rm -d --
 
 pushd /var/www/html &> /dev/null || exit
 
@@ -25,3 +48,16 @@ sudo apt install php-simplexml
 php composer.phar update
 
 popd &> /dev/null || exit
+
+
+echo "=================="
+echo "Running smoke test"
+echo "=================="
+LIVENESS_PROBE="$(curl -I --location --request OPTIONS 'http://localhost:80' -w "%{http_code}" -o /dev/null -s)"
+
+if [ "$LIVENESS_PROBE" != "200" ]; then
+        echo "Health check failed. Expected 200 got $LIVENESS_PROBE"
+       exit 1
+fi
+
+echo "Successfully deployed gingerberry! :)"
